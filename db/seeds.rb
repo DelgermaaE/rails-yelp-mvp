e2b6ee2c3db19443df99adbc3dfa2a9fb2353f15
delgermaa erkhembayar <edelgerma@gmail.com>
2018-11-07 15:32:26 +0900

    Task 2 yelp review understanding 2 model relations and display'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create([{ name: 'aljdifj' }, { address: 'adfa' },
+                   { phone_number: '123' }, { category: 'chinese' }])
+Restaurant.create([{ name: 'adfadf' }, { address: 'lojsf' },
+                   { phone_number: '546' }, { category: 'italian' }])
+Restaurant.create([{ name: 'ghndhgj' }, { address: 'molih' },
+                   { phone_number: '596' }, { category: 'japanese' }])
+Restaurant.create([{ name: 'ikyuoi' }, { address: 'uslnf' },
+                   { phone_number: '544' }, { category: 'french' }])
+Restaurant.create([{ name: 'piolk' }, { address: 'kmly' },
+                   { phone_number: '235' }, { category: 'belgian' }])
